4daeb3d35e5ccafece2eeb23ae46f5e3c9cad6d0
ATemunovic <atemunovic@Allens-MBP.attlocal.net>
2020-02-07 12:17:10 -0800

    SASS setup & homepage

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hompage from "../components/Hompage/Hompage";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Hompage />
+    </div>
+  );
+}
+
+export default App;
